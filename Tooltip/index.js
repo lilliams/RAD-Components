34fbd8216bc6c94cc25b9aef5a6ea5e4baddb185
redroot <luke@red-root.com>
2020-02-01 18:16:06 +0000

    add variables and remove extra boolean checks, adds  comments

--- Tooltip/index.js
+++ Tooltip/index.js
@@ -1,46 +1,52 @@
+// document.addEventListener('DOMContentLoaded', )
 $(document).ready(function(){ 
 
-    $('#tooltip-btn').click(function (){
+    // part 1
+    // const = constant - i dont want this to change. 
+    // let = this can change e.g an array;
+    // get the variables you need first at the top of the file.
+    // a) get elements as variables from the DOM
+    // b) define event listeners;
+    // part 2: an if check evaluates stuff as true or false by default
+    // if you see === true / false, you probably dont need.
+    const tooltipBtn = $('#tooltip-btn');
+    const tooltipContent = $('.tooltip-content');
+    const elementsWithinTooltip = $('.tooltip-content, .tooltip-msg');
+    const closeBtn = $('#close-btn');
+
+    tooltipBtn.click(function (){
         // remove hidden class from tooltip-content
-        $('.tooltip-content').toggleClass('hidden');
+        tooltipContent.toggleClass('hidden');
         // if aria-expanded=false, set to true. else change to false
-        if ($('#tooltip-btn').attr('aria-expanded') === 'false') {
-            $('#tooltip-btn').attr('aria-expanded', 'true')
+        if (tooltipBtn.attr('aria-expanded') === 'false') {
+            tooltipBtn.attr('aria-expanded', 'true')
         } else {
-            $('#tooltip-btn').attr('aria-expanded', 'false')
+            tooltipBtn.attr('aria-expanded', 'false')
         };
     });
 
-    $('#close-btn').click(function (){
-        $('.tooltip-content').addClass('hidden');
-
-        $('#tooltip-btn').attr('aria-expanded', 'false');
-
-        $('#tooltip-btn').focus();
+    closeBtn.click(function (){
+        tooltipContent.addClass('hidden');
+        tooltipBtn.attr('aria-expanded', 'false');
+        tooltipBtn.focus();
     });
         
     // ESC KEY TO CLOSE DIALOG
     $(document).keydown(function(event){
-    var escKeyDown = (event.keyCode === 27)
-    var tooltipVisible = ($('#tooltip-content').hasClass('hidden') == false)
+        var escKeyDown = (event.keyCode === 27);
+        var tooltipVisible = !tooltipContent.hasClass('hidden');
         if ( escKeyDown && tooltipVisible ) {
-            $('.tooltip-content').addClass('hidden');
-
-            $('#tooltip-btn').attr('aria-expanded', 'false');
-
-            $('#tooltip-btn').focus();
+            tooltipContent.addClass('hidden');
+            tooltipBtn.attr('aria-expanded', 'false');
+            tooltipBtn.focus();
         };
     });
     
     // MOUSE CLICK OUTSIDE TOOLTIP TO CLOSE
     $(document).mouseup(function(mouseEvent){
-        var tooltipContent = $('.tooltip-content, .tooltip-msg');
-
-        if (!tooltipContent.is(mouseEvent.target) === true){
-
-            $('.tooltip-content').addClass('hidden');
-
-            $('#tooltip-btn').attr('aria-expanded', 'false');  
+        if (!elementsWithinTooltip.is(mouseEvent.target)){
+            tooltipContent.addClass('hidden');
+            tooltipBtn.attr('aria-expanded', 'false');  
         };
     });
     
